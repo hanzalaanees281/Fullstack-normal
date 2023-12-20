67a3d002b0facd81be876a8c9df81504a86d2209
hanzalaanees281 <hanzalaanees555@gmail.com>
2023-12-20 21:02:44 +0500

    fetch api by axios backend to frontend

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import 'dotenv/config'
+import express from 'express';
+
+const app = express();
+
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A joke',
+            content: 'This is a joke'
+        },
+        {
+            id: 2,
+            title: 'Another joke',
+            content: 'This is anohter joke'
+        },
+        {
+            id: 3,
+            title: 'Third joke',
+            content: 'This is a third joke'
+        },
+        {
+            id: 4,
+            title: 'Fourth joke',
+            content: 'This is a fourth joke'
+        },
+        {
+            id: 5,
+            title: 'Fifth joke',
+            content: 'This is a fifth joke'
+        }
+    ]
+    res.send(jokes)
+});
+
+const port = process.env.PORT;
+
+app.listen(port, ()=>{
+    console.log(`server at http://localhost:${port}`);
+})
